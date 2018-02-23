4c18d0e960372d8857eb8fb527159cde7d2109d4
dllmusic <34119160+dllmusic@users.noreply.github.com>
2018-02-23 00:59:54 -0500

    Add files via upload

--- src/moDllz.cpp
+++ src/moDllz.cpp
@@ -14,7 +14,7 @@ void init(rack::Plugin *p) {
 	p->website = "https://github.com/dllmusic/VCV_moDllz";
     // For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
 	p->addModel(createModel<MIDIdualCVWidget>  ("moDllz", "MIDIdualCV",   "MIDI to dual CV interface", MIDI_TAG, DUAL_TAG, UTILITY_TAG));
-    p->addModel(createModel<MIDIPolyWidget>  ("moDllz", "MIDI-Poly",   "MIDI Poly16 Sequencer Arpeggiator", MIDI_TAG, UTILITY_TAG, SEQUENCER_TAG));
+    p->addModel(createModel<MIDIPolyWidget>  ("moDllz", "MIDI-Poly",   "MIDI Poly16 Seq Arpeggiator", MIDI_TAG, UTILITY_TAG, SEQUENCER_TAG));
 //    p->addModel(createModel<MIDIMPE8Widget>  ("moDllz", "MIDI_MPE8",   "MIDI-MPE 8ch interface", MIDI_TAG, UTILITY_TAG));
     p->addModel(createModel<TwinGliderWidget>  ("moDllz", "TwinGlider",   "TwinGlider Dual Portamento", SLEW_LIMITER_TAG,  DUAL_TAG, ENVELOPE_FOLLOWER_TAG, UTILITY_TAG));
 }
